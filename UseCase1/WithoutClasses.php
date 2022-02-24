bba61bc5d7cac12cd0554379a40b99b05a63de38
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-02-24 09:37:40 +0100

    Create array for items in basket for the non-class exercise

---
+++ UseCase1/WithoutClasses.php
@@ -0,0 +1,11 @@
+<?php
+
+// Create an array for the items in the basket
+$nutrition = [
+    ['name' => 'banana', 'amount' => '6', 'price' => 1, 'alcohol' => false],
+    ['name' => 'apple', 'amount' => '3', 'price' => 1.5, 'alcohol' => false],
+    ['name' => 'wine bottle', 'amount' => '2', 'price' => 10, 'alcohol' => true]
+];
+
+// TODO: Calculate the total price
+// TODO: Calculate how much of the total price is tax (fruit= 6%, wine= 21%)
