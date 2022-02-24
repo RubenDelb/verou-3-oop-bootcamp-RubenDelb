db72d6145db01d9f5fa13bc4f942e4d2c9fcecec
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-02-24 12:26:22 +0100

    Make the same basket with the OOP

---
+++ UseCase1/WithClasses.php
@@ -0,0 +1,46 @@
+<?php
+
+function pre($input)
+{
+    echo "<pre>";
+    var_dump($input);
+    echo "</pre>";
+}
+
+class Product
+{
+    public string $name;
+    public int $amount;
+    public float $price;
+    public bool $alcoholOrNot;
+
+    function __construct(string $name, int $amount, float $price, bool $alcoholOrNot = false)
+    {
+        $this->name = $name;
+        $this->amount = $amount;
+        $this->price = $price;
+        $this->alcoholOrNot = $alcoholOrNot;
+    }
+
+    public function getPrice()
+    {
+        return $this->amount * $this->price;
+    }
+
+    public function getTax()
+    {
+        if ($this->alcoholOrNot) {
+            return $this->getPrice() * 0.21;
+        } else {
+            return $this->getPrice() * 0.06;
+        }
+    }
+}
+
+$banana = new Product('banana', 6, 1);
+$apple = new Product('apple', 3, 1.5);
+$wine = new Product('wine', 2, 10, true);
+
+echo "Total Price: €" . ($banana->getPrice() + $apple->getPrice() + $wine->getPrice()) . "<br>";
+
+echo "The total paid taxes on all your products is: €" . ($banana->getTax() + $apple->getTax() + $wine->getTax());
